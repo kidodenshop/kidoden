2a749fc11a92e6651d841a9667a845cbf3cddcac
Durga Prasad Hota <durgaprasadhota@Durgas-MacBook-Air.local>
2026-05-08 14:42:13 +0530

    fix: strip protocol from robots.txt Host directive

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,9 @@
 module.exports = {
   siteUrl: 'https://kidoden.in',
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    transformRobotsTxt: async (_, robotsTxt) => {
+      return robotsTxt.replace('Host: https://kidoden.in', 'Host: kidoden.in');
+    },
+  },
 }
